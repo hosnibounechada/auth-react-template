989be3d9d0980d45b0d279fd1174aba7206e3931
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-09-30 23:08:08 +0100

    navigation using react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/nav-bar";
+import Home from "./pages/home";
+import Landing from "./pages/landing-page";
+import Login from "./pages/login-page";
 import Register from "./pages/register";
 
 function App() {
-  return <Register />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,7 @@ const features = [
   { name: "Considerations", description: "Made from natural materials. Grain and color vary with each item." },
 ];
 
-const Landing = () => {
+const Home = () => {
   return (
     <div className="bg-white">
       <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
@@ -54,4 +54,4 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+export default Home;
