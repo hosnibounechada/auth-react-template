13b3d6f1cc2d824fba01eddc608fb8780994beb7
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-11-09 01:22:23 +0100

    add users state to the chat context

--- src/context/chat-provider.tsx
+++ src/context/chat-provider.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import { useRequestPrivate } from "../hooks";
 
 type ChatContextProps = {
   children: React.ReactNode;
@@ -18,7 +19,33 @@ interface User {
   lastMessage: string;
 }
 
+interface UserAPI {
+  displayName: string;
+  id: string;
+  lastMessage: string;
+  sender: string;
+  status: boolean;
+  thumbnail: string;
+  updatedAt: Date;
+  viewed: boolean;
+}
+
+interface UsersObj {
+  [id: string]: {
+    id: string;
+    sender: string;
+    displayName: string;
+    thumbnail: string;
+    lastMessage: string;
+    time: Date;
+    viewed: boolean;
+    status: boolean;
+  };
+}
+
 const ChatContext = createContext<{
+  users: UsersObj;
+  setUsers: React.Dispatch<React.SetStateAction<UsersObj>>;
   user: User | null;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
   messages: Message[];
@@ -26,6 +53,8 @@ const ChatContext = createContext<{
   page: number;
   setPage: React.Dispatch<React.SetStateAction<number>>;
 }>({
+  users: {},
+  setUsers: () => {},
   user: null,
   setUser: () => {},
   messages: [],
@@ -35,11 +64,38 @@ const ChatContext = createContext<{
 });
 
 export const MessagesProvider = ({ children }: ChatContextProps) => {
+  const [users, setUsers] = useState<UsersObj>({});
+  const { doRequestPrivate: doGetFriends } = useRequestPrivate({ url: "/users/messages/friendsMessages", method: "get" });
+
   const [user, setUser] = useState<User | null>(null);
   const [messages, setMessages] = useState<Message[]>([]);
   const [page, setPage] = useState<number>(0);
 
-  return <ChatContext.Provider value={{ user, setUser, messages, setMessages, page, setPage }}>{children}</ChatContext.Provider>;
+  const getUsers = async () => {
+    const { result } = await doGetFriends();
+    const res = result.reduce((r: UsersObj, e: UserAPI) => {
+      r[e.id] = {
+        id: e.id,
+        sender: e.sender,
+        displayName: e.displayName,
+        thumbnail: e.thumbnail,
+        lastMessage: e.lastMessage,
+        time: e.updatedAt,
+        viewed: e.viewed,
+        status: e.status,
+      };
+      return r;
+    }, {});
+    setUsers({ ...res });
+    if (result.length > 0) setUser(result[0]);
+  };
+
+  useEffect(() => {
+    console.log("initial useEffect");
+    getUsers();
+  }, []);
+
+  return <ChatContext.Provider value={{ users, setUsers, user, setUser, messages, setMessages, page, setPage }}>{children}</ChatContext.Provider>;
 };
 
 export default ChatContext;
